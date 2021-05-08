55c0505ded3f410112fb3dffa811a48083f66666
MustafaTunahanTuna <mustafatunahantuna@yandex.com>
2021-05-08 04:03:02 +0300

    Admin LTE 3.0.4 Teması Giydirildi

---
+++ MvcProjeKamp/Controllers/AdminCategoryController.cs
@@ -0,0 +1,48 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKamp.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        // GET: AdminCategory
+        public ActionResult Index()
+        {
+            var categories = cm.GetList();
+            return View(categories);
+        }
+        [HttpGet]
+       public ActionResult addCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult addCategory(Category p)
+        {
+            CategoryValidator categorValidator = new CategoryValidator();
+            ValidationResult result = categorValidator.Validate(p);
+            if (result.IsValid)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName,item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
