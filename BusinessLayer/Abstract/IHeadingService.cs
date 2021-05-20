b3b54cb920f3ff6e77d213226fd8e6cf601eba82
MustafaTunahanTuna <mustafatunahantuna@yandex.com>
2021-05-20 23:29:34 +0300

    Başlıklisteleme ile alakalı basit bir arayüz gerçekleştirildi. Başlık için bussinis layerda Manager classı oluşturuldu

---
+++ BusinessLayer/Abstract/IHeadingService.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IHeadingService
+    {
+        List<Heading> GetList();
+
+        void HeadingAdd(Heading heading);
+
+        void HeadingDelete(Heading heading);
+
+        void HeadingUpdate(Heading heading);
+        Heading getById(int id);
+    }
+}
